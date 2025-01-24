da8c150ec72a48dc3bdce0b1fc26f4deb5d5ccbd
yscorsato <yscorsato@gmail.com>
2025-01-24 15:01:51 -0300

    Resolvi o problema de repetição, agora tenho que arrumar caso alguém coloque números no lugar do nome.

--- urna.py
+++ urna.py
@@ -1,13 +1,14 @@
 while True:
     try:
         candidato1 = str(input("Canditato1: "))
-        continue
+        break
     except ValueError:
         print('Erro ao cadastar')
         break
 while True:
     try:
         candidato2 = str(input("Canditato2: "))
+        break
     except ValueError:
         print('Erro ao cadastar')
         break
