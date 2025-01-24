8944a18575fec96eea201849958f1df8ac9a9a31
yscorsato <yscorsato@gmail.com>
2025-01-24 15:56:22 -0300

    Resolvendo outro ploblema vou agora pegar o número dos nossos candidatos

--- urna.py
+++ urna.py
@@ -1,6 +1,6 @@
 while True:
     try:
-        candidato1 = input("Candidato1: ")
+        candidato1 = input("Candidato 1: ")
         if not candidato1.isalpha():
             raise ValueError("Erro: Apenas nomes são permitidos.")
         break
@@ -8,7 +8,7 @@ while True:
         print(e)
 while True:
     try:
-        candidato2 = input("Candidato2: ")
+        candidato2 = input("Candidato 2: ")
         if not candidato2.isalpha():
             raise ValueError("Erro: Apenas nomes são permitidos.")
         break
@@ -16,3 +16,22 @@ while True:
         print(e)
 
 print('')
+
+while True:
+    try:
+        candidato1 = input("Partido do candidato 1: ")
+        if not candidato1.isalpha():
+            raise ValueError("Erro: Apenas nomes são permitidos.")
+        break
+    except ValueError as e:
+        print(e)
+while True:
+    try:
+        candidato2 = input("Partido do candidato 2: ")
+        if not candidato2.isalpha():
+            raise ValueError("Erro: Apenas nomes são permitidos.")
+        break
+    except ValueError as e:
+        print(e)
+print(' ')
+
