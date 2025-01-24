107343c5af9e47b4337ff98106c04b4754347741
yscorsato <yscorsato@gmail.com>
2025-01-24 15:34:26 -0300

    Resolvi o problema dos números, agora vou para a segunda parte de cadastrar o partido deles e o número dos candidatos, nome dos vice dos 2 candidatos

--- urna.py
+++ urna.py
@@ -1,15 +1,18 @@
 while True:
     try:
-        candidato1 = str(input("Canditato1: "))
+        candidato1 = input("Candidato1: ")
+        if not candidato1.isalpha():
+            raise ValueError("Erro: Apenas nomes são permitidos.")
         break
-    except ValueError:
-        print('Erro ao cadastar')
-        break
+    except ValueError as e:
+        print(e)
 while True:
     try:
-        candidato2 = str(input("Canditato2: "))
+        candidato2 = input("Candidato2: ")
+        if not candidato2.isalpha():
+            raise ValueError("Erro: Apenas nomes são permitidos.")
         break
-    except ValueError:
-        print('Erro ao cadastar')
-        break
-    print('')
+    except ValueError as e:
+        print(e)
+
+print('')
